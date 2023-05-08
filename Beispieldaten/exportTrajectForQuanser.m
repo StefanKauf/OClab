6a29bea2521bb7843ffbfb6595bf5f5e115c3f3b
Stefan <kaufmannstefan@outlook.com>
2023-05-08 20:32:55 +0200

    add Update + Export + ulim

--- Beispieldaten/exportTrajectForQuanser.m
+++ Beispieldaten/exportTrajectForQuanser.m
@@ -1,22 +1,47 @@
-function [t_interp, phi_interp, u_interp] = exportTrajectForQuanser(...
-    phi_in, omega_in, u_in, t_in, dt_controller, filename, folder)
+function [t_interp, phi_1_interp, u_interp] = exportTrajectForQuanser(...
+    phi_1_in, omega_1_in, u_in, t_in, dt_controller, filename, folder,...
+    current_in, phi_2_in, omega_2_in)
 %exportTrajectForQuanser Export trajectory for quanser testrig
 % Automatic correction of timesteps according to dt_controller
 
-%   Z_in, t_in  ... Position and time of slider (can be random timestep)
+%   phi_1_in, omega_1_in, u_in, t_in  ... Position [rad], Velocity [rad/s]
+%       of first angle; FeedForward voltage [V] (can be random timestep)
 %   dt_controller [s]   ... timestep of controller for export of trajectory
 %   filename and folder ... place where the trajectory should be safed and
 %                           name of the file; ".txt" does not need to be
 %                           provided
+%   current_in, phi_2_in, omega_2_in    ... Optional to provide (if
+%       provided, the plots on quanser are correct (ref exists)); required
+%       for certain types of controllers (Full-StateFeedback)
 
 %% Calculate time and interpolate trajectory
 % Calculate timeline
 t_interp = 0:dt_controller:t_in(end)+dt_controller;
 
 % Interpolate trajectory to get the intermediary points
-phi_interp = interp1(t_in, phi_in, t_interp, 'pchip', phi_in(end));
-omega_interp = interp1(t_in, omega_in, t_interp, 'pchip', omega_in(end));
+phi_1_interp = interp1(t_in, phi_1_in, t_interp, 'pchip', phi_1_in(end));
+omega_1_interp = interp1(t_in, omega_1_in, t_interp, 'pchip', omega_1_in(end));
 u_interp = interp1(t_in, u_in, t_interp, 'pchip', 0);
+
+% If provided, the following are exported as well
+if nargin > 7
+    current_interp = interp1(t_in, current_in, t_interp, 'pchip', 0);
+    if nargin > 8
+        phi_2_interp = interp1(t_in, phi_2_in, t_interp, 'pchip', phi_2_in(end));
+        if nargin > 9
+            omega_2_interp = interp1(t_in, omega_2_in, t_interp, 'pchip', omega_2_in(end));
+        else
+            omega_2_interp = zeros(size(phi_1_interp));
+        end
+    else
+        phi_2_interp = zeros(size(phi_1_interp));
+        omega_2_interp = zeros(size(phi_1_interp));
+    end
+else
+    current_interp = zeros(size(phi_1_interp));
+    phi_2_interp = zeros(size(phi_1_interp));
+    omega_2_interp = zeros(size(phi_1_interp));
+end
 
 %% Export of Trajectory
 % Check if filename was provided and display warning if not
@@ -62,25 +87,46 @@ fileID = fopen(ExportString,'w');
 
 % Print first lines that are required
 nbytes = fprintf(fileID, "// Define Variables: \n");
-nbytes = fprintf(fileID, "float phi_ref[%d];\n", length(phi_interp));
-nbytes = fprintf(fileID, "float omega_ref[%d];\n", length(u_interp));
-nbytes = fprintf(fileID, "float u_ref[%d];\n", length(u_interp));
+nbytes = fprintf(fileID, "float phi_1_ref[%d];\n", length(phi_1_interp));
+nbytes = fprintf(fileID, "float phi_2_ref[%d];\n", length(phi_1_interp));
+nbytes = fprintf(fileID, "float omega_1_ref[%d];\n", length(phi_1_interp));
+nbytes = fprintf(fileID, "float omega_2_ref[%d];\n", length(phi_1_interp));
+nbytes = fprintf(fileID, "float i_ref[%d];\n", length(phi_1_interp));
+nbytes = fprintf(fileID, "float u_ref[%d];\n", length(phi_1_interp));
 
 nbytes = fprintf(fileID, " \n");
 % Print phi_ref trajectory
-for index0 = 0:(length(phi_interp)-1)
-    nbytes = fprintf(fileID, "phi_ref[%d] = %.8f;\n", index0, phi_interp(index0+1));
+for index = 0:(length(phi_1_interp)-1)
+    nbytes = fprintf(fileID, "phi_1_ref[%d] = %.8f;\n", index, phi_1_interp(index+1));
 end
 
 nbytes = fprintf(fileID, " \n");
 % Print omgega_ref trajectory
-for index = 0:(length(u_interp)-1)
-    nbytes = fprintf(fileID, "omega_ref[%d] = %.8f;\n", index, omega_interp(index+1)); 
+for index = 0:(length(phi_1_interp)-1)
+    nbytes = fprintf(fileID, "omega_1_ref[%d] = %.8f;\n", index, omega_1_interp(index+1)); 
+end
+
+nbytes = fprintf(fileID, " \n");
+% Print phi_ref trajectory
+for index = 0:(length(phi_1_interp)-1)
+    nbytes = fprintf(fileID, "phi_2_ref[%d] = %.8f;\n", index, phi_2_interp(index+1));
+end
+
+nbytes = fprintf(fileID, " \n");
+% Print omgega_ref trajectory
+for index = 0:(length(phi_1_interp)-1)
+    nbytes = fprintf(fileID, "omega_2_ref[%d] = %.8f;\n", index, omega_2_interp(index+1)); 
 end
 
 nbytes = fprintf(fileID, " \n");
 % Print u_ref trajectory
-for index = 0:(length(u_interp)-1)
+for index = 0:(length(phi_1_interp)-1)
+    nbytes = fprintf(fileID, "i_ref[%d] = %.8f;\n", index, current_interp(index+1)); 
+end
+
+nbytes = fprintf(fileID, " \n");
+% Print u_ref trajectory
+for index = 0:(length(phi_1_interp)-1)
     nbytes = fprintf(fileID, "u_ref[%d] = %.8f;\n", index, u_interp(index+1)); 
 end
 
@@ -90,10 +136,16 @@ nbytes = fprintf(fileID, "// Define variables to export \n");
 nbytes = fprintf(fileID, "k_lauf_max = %d; // Needs to be the highest index from u_ref and phi_ref \n", index);
 nbytes = fprintf(fileID, " \n");
 nbytes = fprintf(fileID, "// Actually export trajectories \n");
-nbytes = fprintf(fileID, "phi_out = phi_ref[idx_requested]; \n");
-nbytes = fprintf(fileID, "omega_out = phi_ref[idx_requested]; \n");
+nbytes = fprintf(fileID, "phi_1_out = phi_1_ref[idx_requested]; \n");
+nbytes = fprintf(fileID, "phi_2_out = phi_2_ref[idx_requested]; \n");
+nbytes = fprintf(fileID, "omega_1_out = omega_1_ref[idx_requested]; \n");
+nbytes = fprintf(fileID, "omega_2_out = omega_2_ref[idx_requested]; \n");
+nbytes = fprintf(fileID, "i_out = i_ref[idx_requested]; \n");
 nbytes = fprintf(fileID, "u_out = u_ref[idx_requested]; \n");
-    
+nbytes = fprintf(fileID, " \n");
+nbytes = fprintf(fileID, "// Export Endpoints \n");
+nbytes = fprintf(fileID, "phi_1_end = phi_1_ref[%d]; \n", index);
+nbytes = fprintf(fileID, "phi_2_end = phi_2_ref[%d]; \n", index);
 % nbytes = fprintf(fileID, "gk_end = %d;", index);
 
 fclose(fileID);
--- libopc.py
+++ libopc.py
@@ -12,7 +12,7 @@ Nützliche Funktionen welche häufig verwendet werden.
 import numpy as np
 import Parameter as param
 import matplotlib as plt
-
+import datetime
 
 
 ######## ***************************************  
@@ -63,9 +63,9 @@ def runge_kutta_k4(f,x,u,h=1):
 ## Systemdynamik vereinfachtes Lineares Modell
 ######## ***************************************  
 
-A_simple = np.array([[-param.R/param.L, 0, -(param.km*param.kg)/param.L],
+A_simple = np.array([[-param.R/param.L, 0, -(param.km)/(param.L*param.kg)],
               [0,                0,                          1  ],
-              [param.ng*param.nm*param.km/param.Jeq,0,-param.b1/param.Jeq]])
+              [param.ng*param.nm*param.km*param.kg/param.Jeq,0,-param.b1/param.Jeq]])
 
 B_simple = np.array([1/param.L,
               0,
@@ -74,3 +74,171 @@ C_simple = np.array([0,1,0])
 D_simple = 0
 
 f_simple_dynamic = lambda x,u: A_simple@x+B_simple*u
+
+
+
+######## ***************************************  
+## Export Trajektorie
+######## ***************************************  
+
+def exportTrajectForQuanser_old(phi_in, omega_in, u_in, t_in, dt_controller, filename, folder=None):
+    # Calculate time and interpolate trajectory
+    t_interp = np.arange(0, t_in[-1]+dt_controller, dt_controller)
+
+    # Interpolate trajectory to get the intermediary points
+    phi_interp = np.interp(t_interp, t_in, phi_in)
+    omega_interp = np.interp(t_interp, t_in, omega_in)
+    u_interp = np.interp(t_interp, t_in, u_in, left=0)
+
+    # Export of Trajectory
+    if filename is None:
+        import datetime
+        filename = datetime.datetime.now().strftime("%Y%m%d__%H%M%S_") + 'traject' + '.txt'
+    elif not filename.endswith('.txt'):
+        filename += '.txt'
+
+    if folder is not None:
+        filename = folder + '/' + filename
+
+    # Actually export
+    with open(filename, 'w') as file:
+        file.write("// Define Variables: \n")
+        file.write("float phi_ref[{}];\n".format(len(phi_interp)))
+        file.write("float omega_ref[{}];\n".format(len(omega_interp)))
+        file.write("float u_ref[{}];\n".format(len(u_interp)))
+        file.write("\n")
+
+        # Print phi_ref trajectory
+        for index, phi in enumerate(phi_interp):
+            file.write("phi_ref[{}] = {:.8f};\n".format(index, phi))
+        file.write("\n")
+
+        # Print omega_ref trajectory
+        for index, omega in enumerate(omega_interp):
+            file.write("omega_ref[{}] = {:.8f};\n".format(index, omega))
+        file.write("\n")
+
+        # Print u_ref trajectory
+        for index, u in enumerate(u_interp):
+            file.write("u_ref[{}] = {:.8f};\n".format(index, u))
+        file.write("\n")
+
+        # Print the last lines that are required
+        file.write("// Define variables to export \n")
+        file.write("k_lauf_max = {}; // Needs to be the highest index from u_ref and phi_ref \n".format(len(u_interp) - 1))
+        file.write("\n")
+        file.write("// Actually export trajectories \n")
+        file.write("phi_out = phi_ref[idx_requested]; \n")
+        file.write("omega_out = phi_ref[idx_requested]; \n")
+        file.write("u_out = u_ref[idx_requested]; \n")
+
+
+
+def exportTrajectForQuanser(phi_1_in, omega_1_in, u_in, t_in, dt_controller, filename, folder=None,
+                            current_in=None, phi_2_in=None, omega_2_in=None):
+    
+    # Calculate time and interpolate trajectory
+    # Calculate timeline
+    t_interp = np.arange(0, t_in[-1]+dt_controller, dt_controller)
+    
+    # Interpolate trajectory to get the intermediary points
+    phi_1_interp = np.interp(t_interp, t_in, phi_1_in)
+    omega_1_interp = np.interp(t_interp, t_in, omega_1_in)
+    u_interp = np.interp(t_interp, t_in, u_in)
+    
+    # If provided, the following are exported as well
+    if current_in is not None:
+        current_interp = np.interp(t_interp, t_in, current_in)
+        if phi_2_in is not None:
+            phi_2_interp = np.interp(t_interp, t_in, phi_2_in)
+            if omega_2_in is not None:
+                omega_2_interp = np.interp(t_interp, t_in, omega_2_in)
+            else:
+                omega_2_interp = np.zeros_like(phi_1_interp)
+        else:
+            phi_2_interp = np.zeros_like(phi_1_interp)
+            omega_2_interp = np.zeros_like(phi_1_interp)
+    else:
+        current_interp = np.zeros_like(phi_1_interp)
+        phi_2_interp = np.zeros_like(phi_1_interp)
+        omega_2_interp = np.zeros_like(phi_1_interp)
+    
+    # Export of Trajectory
+    # Check if filename was provided and display warning if not
+    if filename is None:
+        import warnings
+        warnings.warn('No filename for export was provided - date used as filename')
+        ExportString = 'traject'
+    else:
+        ExportString = filename
+    
+    # Check if ".txt" exists
+    if not ExportString.endswith('.txt'):
+        ExportString = ExportString + '.txt'
+    
+    ExportString = datetime.datetime.now().strftime("%Y%m%d__%H%M%S_") + ExportString
+    
+    # Add folder to name - if provided
+    if folder is not None:
+        ExportString = folder + '/' + ExportString
+    
+    # Actually export
+    fileID = open(ExportString, 'w')
+    
+    # Print first lines that are required
+    fileID.write("// Define Variables: \n")
+    fileID.write("float phi_1_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("float phi_2_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("float omega_1_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("float omega_2_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("float i_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("float u_ref[{}];\n".format(len(phi_1_interp)))
+    fileID.write("\n")
+        
+    # Print omgega_ref trajectory
+    for index, value in enumerate(omega_1_interp):
+        fileID.write("omega_1_ref[{}] = {};\n".format(index, value))
+
+    fileID.write("\n")
+    # Print phi_ref trajectory
+    for index, value in enumerate(phi_2_interp):
+        fileID.write("phi_2_ref[{}] = {};\n".format(index, value))
+
+    fileID.write("\n")
+    # Print omgega_ref trajectory
+    for index, value in enumerate(omega_2_interp):
+        fileID.write("omega_2_ref[{}] = {};\n".format(index, value))
+
+    fileID.write("\n")
+    # Print u_ref trajectory
+    for index, value in enumerate(current_interp):
+        fileID.write("i_ref[{}] = {};\n".format(index, value))
+
+    fileID.write("\n")
+    # Print u_ref trajectory
+    for index, value in enumerate(u_interp):
+        fileID.write("u_ref[{}] = {};\n".format(index, value))
+
+    # Print the last lines that are required
+    fileID.write("\n")
+    fileID.write("// Define variables to export \n")
+    fileID.write("k_lauf_max = {}; // Needs to be the highest index from u_ref and phi_ref \n".format(index))
+
+    fileID.write("\n")
+    fileID.write("// Actually export trajectories \n")
+    fileID.write("phi_1_out = phi_1_ref[idx_requested]; \n")
+    fileID.write("phi_2_out = phi_2_ref[idx_requested]; \n")
+    fileID.write("omega_1_out = omega_1_ref[idx_requested]; \n")
+    fileID.write("omega_2_out = omega_2_ref[idx_requested]; \n")
+    fileID.write("i_out = i_ref[idx_requested]; \n")
+    fileID.write("u_out = u_ref[idx_requested]; \n")
+
+    fileID.write("\n")
+    fileID.write("// Export Endpoints \n")
+    fileID.write("phi_1_end = phi_1_ref[{}]; \n".format(index))
+    fileID.write("phi_2_end = phi_2_ref[{}]; \n".format(index))
+
+    fileID.close()
+
+
+
